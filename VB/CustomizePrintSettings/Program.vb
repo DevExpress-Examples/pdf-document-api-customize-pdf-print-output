0fde3d6ee0a6b6a3631e5dcc1ccbf4f1d8dd0d37
DevExpressExampleBot <fakeEmail>
2022-02-01 19:24:33 +0300

    Source auto update [skip ci]

--- VB/CustomizePrintSettings/Program.vb
+++ VB/CustomizePrintSettings/Program.vb
@@ -1,29 +1,24 @@
-﻿Imports DevExpress.Pdf
+Imports DevExpress.Pdf
 Imports System.Drawing
 
 Namespace CustomizePrintSettings
+
     Friend Class Program
-        Shared Sub Main(ByVal args() As String)
 
+        Shared Sub Main(ByVal args As String())
             ' Create a PDF Document Processor instance and load a PDF into it.
-            Using documentProcessor As New PdfDocumentProcessor()
+            Using documentProcessor As PdfDocumentProcessor = New PdfDocumentProcessor()
                 documentProcessor.LoadDocument("..\..\Demo.pdf")
-
                 ' Declare the PDF printer settings.
-                Dim settings As New PdfPrinterSettings()
-
+                Dim settings As PdfPrinterSettings = New PdfPrinterSettings()
                 ' Specify the page numbers to be printed.
-                settings.PageNumbers = New Integer() { 1, 2, 3, 4, 5, 6 }
-
+                settings.PageNumbers = New Integer() {1, 2, 3, 4, 5, 6}
                 ' Handle the PrintPage event to specify print output.
                 AddHandler documentProcessor.PrintPage, AddressOf OnPrintPage
-
                 ' Handle the QueryPageSettings event to customize settings for a page to be printed.
                 AddHandler documentProcessor.QueryPageSettings, AddressOf OnQueryPageSettings
-
                 ' Print the document using the specified printer settings.
                 documentProcessor.Print(settings)
-
                 ' Unsubscribe from PrintPage and QueryPageSettings events. 
                 RemoveHandler documentProcessor.PrintPage, AddressOf OnPrintPage
                 RemoveHandler documentProcessor.QueryPageSettings, AddressOf OnQueryPageSettings
@@ -41,10 +36,8 @@ Namespace CustomizePrintSettings
 
         ' Specify what happens when the PrintPage event is raised.
         Private Shared Sub OnPrintPage(ByVal sender As Object, ByVal e As PdfPrintPageEventArgs)
-
             ' Draw a picture on each printed page.        
-            Using image As New Bitmap("..\..\DevExpress.png")
-
+            Using image As Bitmap = New Bitmap("..\..\DevExpress.png")
                 e.Graphics.DrawImage(image, New RectangleF(10, 30, image.Width \ 2, image.Height \ 2))
             End Using
         End Sub
